fa349f20f3e696826504d7bfaa115a2157bb0b45
David Holmes <dholmes@dholmes.us>
2020-08-08 14:33:55 -0700

    Implement C++/WinRT "Hello World"
    Tutorial project from: https://docs.microsoft.com/en-us/windows/uwp/get-started/create-a-basic-windows-10-app-in-cppwinrt

---
+++ HelloWorldCppWinRT/MainPage.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include "MainPage.g.h"
+
+namespace winrt::HelloWorldCppWinRT::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+
+        void ClickHandler(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& args);
+
+        void inputButton_Click(
+            winrt::Windows::Foundation::IInspectable const& sender,
+            winrt::Windows::UI::Xaml::RoutedEventArgs const& e);
+    };
+}
+
+namespace winrt::HelloWorldCppWinRT::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
